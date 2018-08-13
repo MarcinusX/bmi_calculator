9c028df739be1417dcf4aa3be4a34d61e2f3fee0
MarcinusX <marcin.szalek1@gmail.com>
2018-08-13 09:03:18 +0200

    Added gender icons

--- lib/gender_card.dart
+++ lib/gender_card.dart
@@ -1,6 +1,7 @@
 import 'dart:math' as math;
 
 import 'package:bmi_calculator/card_title.dart';
+import 'package:bmi_calculator/gender.dart';
 import 'package:flutter/material.dart';
 
 class GenderCard extends StatefulWidget {
@@ -9,6 +10,20 @@ class GenderCard extends StatefulWidget {
 }
 
 class _GenderCardState extends State<GenderCard> {
+  static final double _circleSize = 80.0;
+  static final double _arrowLength = 32.0;
+  static final double _defaultGenderAngle = 43.0;
+  static final Map<Gender, double> _genderAngles = {
+    Gender.female: -_defaultGenderAngle,
+    Gender.other: 0.0,
+    Gender.male: _defaultGenderAngle,
+  };
+  static final Map<Gender, String> _genderImages = {
+    Gender.female: "images/gender_female.png",
+    Gender.other: "images/gender_other.png",
+    Gender.male: "images/gender_male.png",
+  };
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -20,7 +35,10 @@ class _GenderCardState extends State<GenderCard> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               CardTitle("GENDER"),
-              _drawIndicator(),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0),
+                child: _drawMainStack(),
+              ),
             ],
           ),
         ),
@@ -28,20 +46,55 @@ class _GenderCardState extends State<GenderCard> {
     );
   }
 
-  Widget _drawIndicator() {
+  Widget _drawGenderIcon(Gender gender) {
+    double angleInRadians = _degreeToRadians(_genderAngles[gender]);
+    String assetName = _genderImages[gender];
+    double iconHeight = gender == Gender.other ? 22.0 : 16.0;
+    return Padding(
+      padding: EdgeInsets.only(bottom: _circleSize / 2),
+      child: Transform.rotate(
+        alignment: Alignment.bottomCenter,
+        angle: angleInRadians,
+        child: Padding(
+          padding: EdgeInsets.only(bottom: _circleSize / 2 + 24.0),
+          child: Transform.rotate(
+            angle: -angleInRadians,
+            child: Image.asset(
+              assetName,
+              height: iconHeight,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _drawMainStack() {
     return Stack(
-      alignment: Alignment.center,
+      alignment: Alignment.bottomCenter,
       children: <Widget>[
-        _drawCircle(size: 80.0),
-        _drawRotatedArrow(length: 32.0, angle: -43.0),
+        _drawCircleIndicator(),
+        _drawGenderIcon(Gender.female),
+        _drawGenderIcon(Gender.other),
+        _drawGenderIcon(Gender.male),
       ],
     );
   }
 
-  Widget _drawCircle({double size}) {
+  Widget _drawCircleIndicator() {
+    return Stack(
+      alignment: Alignment.center,
+      children: <Widget>[
+        _drawCircle(),
+        _drawRotatedArrow(angle: -43.0),
+      ],
+    );
+  }
+
+  Widget _drawCircle() {
     return Container(
-      width: size,
-      height: size,
+      width: _circleSize,
+      height: _circleSize,
       decoration: BoxDecoration(
         shape: BoxShape.circle,
         color: Color.fromRGBO(244, 244, 244, 1.0),
@@ -49,27 +102,26 @@ class _GenderCardState extends State<GenderCard> {
     );
   }
 
-  Widget _drawRotatedArrow({double length, double angle}) {
+  Widget _drawRotatedArrow({double angle}) {
     return Transform.rotate(
       angle: _degreeToRadians(angle),
-      child: _drawCenteredArrow(length: length),
+      child: _drawCenteredArrow(),
     );
   }
 
-  Widget _drawCenteredArrow({double length}) {
+  Widget _drawCenteredArrow() {
     return Transform.translate(
-      offset: Offset(0.0, -length / 2),
-      child: _drawArrow(length: length),
+      offset: Offset(0.0, -_arrowLength * 0.4),
+      child: _drawArrow(),
     );
   }
 
-  Widget _drawArrow({double length}) {
-    double defaultRotationFromAsset = 43.0;
+  Widget _drawArrow() {
     return Transform.rotate(
-      angle: _degreeToRadians(-defaultRotationFromAsset),
+      angle: _degreeToRadians(-_defaultGenderAngle),
       child: Image.asset(
         "images/gender_indicator.png",
-        height: length,
+        height: _arrowLength,
       ),
     );
   }
